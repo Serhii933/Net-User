3a466b6d619ab569b24174450cc0fe8ebbccc3e3
Serhii933 <Subota933@gmail.com>
2024-04-25 21:31:24 +0300

    Initial commit

---
+++ src/test/java/org/example/netuser/server/NetUserServer.java
@@ -0,0 +1,43 @@
+package org.example.netuser.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public final class NetUserServer {
+
+    static final int PORT = 8001;
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new NetUserServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind(PORT).sync();
+            System.out.println("Server started and waiting for clients...");
+
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
---
+++ src/test/java/org/example/netuser/server/NetUserServerHandler.java
@@ -0,0 +1,35 @@
+package org.example.netuser.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ChannelHandler.Sharable
+public class NetUserServerHandler extends SimpleChannelInboundHandler<String> {
+
+    static final List<Channel> channels = new ArrayList<>();
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) {
+        System.out.println("Client joined - " + ctx);
+        channels.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String msg) {
+        System.out.println("Message received: " + msg);
+        for (Channel c : channels) {
+            c.writeAndFlush("SERVER: " + msg + '\n');
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        System.out.println("Closing connection for client - " + ctx);
+        ctx.close();
+    }
+}
